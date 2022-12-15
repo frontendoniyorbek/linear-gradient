7aac2258f15410b0f9bcc95bc49321916f2e07a3
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-12-15 15:30:32 +0500

    index.html and main.css / main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('body');
+const elBtn = document.querySelector('.btn');
+const elForm = document.querySelector('form');
+
+let linearGradient = () => {
+    const random = Math.trunc(Math.random() * 255 + 1)
+    return random;
+}
+elForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    body.style.backgroundImage = `linear-gradient(43deg, rgb(${linearGradient()},${linearGradient()},${linearGradient()}) ,rgb(${linearGradient()},${linearGradient()},${linearGradient()}),rgb(${linearGradient()},${linearGradient()},${linearGradient()}))`
+    console.log(5);
+})
